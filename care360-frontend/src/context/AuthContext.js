eee22e91af71456d43eeb956df3fac49598508f3
Divjot Singh <divjotsingh983@gmail.com>
2025-05-05 01:23:29 +0530

    Update AuthContext.js

--- care360-frontend/src/context/AuthContext.js
+++ care360-frontend/src/context/AuthContext.js
@@ -29,7 +29,7 @@ export function AuthProvider({ children }) {
       console.log("AuthContext: User profile fetched:", response.data);
     } catch (error) {
       console.error('AuthContext: Failed to fetch user profile:', error.response?.data || error.message);
-      logout(); 
+      //logout(); 
     } finally {
         
     }
