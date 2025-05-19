c1e028979461e70efc37b68e7807dde154077052
Divjot Singh <divjotsingh983@gmail.com>
2025-05-20 04:00:58 +0530

    Update Footer.js

--- care360-frontend/src/components/Footer.js
+++ care360-frontend/src/components/Footer.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import styles from './Footer.module.css'; 
+
 function Footer() {
+  const router = useRouter();
+  const hideOnPaths = ['/chat/booking/${bookingId}']; // Add paths you want to hide footer on
+
+  if (hideOnPaths.includes(router.pathname)) {
+    return null;
+  }
+
   return (
     <footer className={styles.footer}> {/* Apply the footer class */}
       <div className={styles.container}> {/* Apply the container class */}
