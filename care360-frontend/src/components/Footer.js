fbfc4d756f40bfd02dc77c1516dc152fc9fd24c6
Divjot Singh <divjotsingh983@gmail.com>
2025-05-20 04:07:59 +0530

    Update Footer.js

--- care360-frontend/src/components/Footer.js
+++ care360-frontend/src/components/Footer.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import styles from './Footer.module.css'; 
 
 function Footer() {
-  const router = useRouter();
-  const path = router.pathname; // This gives dynamic route pattern e.g. /chat/booking/[bookingId]
-
-  // Hide footer if the path starts with /chat/booking
-  if (path.startsWith('/chat/${bookingId}')) {
-    return null;
-  }
 
   return (
     <footer className={styles.footer}> {/* Apply the footer class */}
